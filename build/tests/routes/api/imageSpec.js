ec2b6c2f6d16e29b7d5cd63e380047d8a7b9e4ba
Ali Nauman <ali.nauman@arbisoft.com>
2022-02-13 20:01:42 +0500

    feat: add additional tests

--- build/tests/routes/api/imageSpec.js
+++ build/tests/routes/api/imageSpec.js
@@ -21,8 +21,41 @@ describe('Image Processing API tests', () => {
         const response = yield request.get(route);
         expect(response.statusCode).not.toBe(404);
     }));
-    it('Checks if server returns 400 if no params provided', () => __awaiter(void 0, void 0, void 0, function* () {
-        const response = yield request.get(route);
-        expect(response.statusCode).toBe(400);
+    it('returns 400 if no params provided', () => __awaiter(void 0, void 0, void 0, function* () {
+        yield request.get(route).expect(400);
     }));
+    it('returns 404 if invalid image provided', () => __awaiter(void 0, void 0, void 0, function* () {
+        yield request.get(route).query({ filename: 'missing.jpg' }).expect(404);
+    }));
+    describe('Tests with valid image', () => {
+        it('returns 200 if valid image provided', () => __awaiter(void 0, void 0, void 0, function* () {
+            yield request.get(route).query({ filename: 'fjord.jpg' }).expect(200);
+        }));
+        it('returns jpeg content type if valid image jpeg provided', () => __awaiter(void 0, void 0, void 0, function* () {
+            yield request.get(route).query({ filename: 'fjord.jpg' }).expect('Content-Type', 'image/jpeg');
+        }));
+        it('returns png content type if valid image png provided', () => __awaiter(void 0, void 0, void 0, function* () {
+            yield request.get(route).query({ filename: '685929.png' }).expect('Content-Type', 'image/png');
+        }));
+        describe('Test with valid resize params', () => {
+            it('returns 200 if valid height and width given', () => __awaiter(void 0, void 0, void 0, function* () {
+                yield request.get(route).query({ filename: 'fjord.jpg', h: '200', w: '200' }).expect(200);
+            }));
+            it('returns png file if valid conversion params given', () => __awaiter(void 0, void 0, void 0, function* () {
+                const queryParams = { filename: 'fjord.jpg', h: '200', w: '200', ext: 'png' };
+                yield request.get(route).query(queryParams).expect(200).expect('Content-Type', 'image/png');
+            }));
+        });
+        describe('Test with invalid resize params', () => {
+            it('returns 400 if invalid height given', () => __awaiter(void 0, void 0, void 0, function* () {
+                yield request.get(route).query({ filename: 'fjord.jpg', h: 'ABC', w: '200' }).expect(400);
+            }));
+            it('returns 400 if invalid width given', () => __awaiter(void 0, void 0, void 0, function* () {
+                yield request.get(route).query({ filename: 'fjord.jpg', h: '200', w: 'ABC' }).expect(400);
+            }));
+            it('returns 400 if extension param given but resize params missing', () => __awaiter(void 0, void 0, void 0, function* () {
+                yield request.get(route).query({ filename: 'fjord.jpg', ext: 'png' }).expect(400);
+            }));
+        });
+    });
 });
--- src/tests/routes/api/imageSpec.ts
+++ src/tests/routes/api/imageSpec.ts
@@ -11,8 +11,50 @@ describe('Image Processing API tests', () => {
         expect(response.statusCode).not.toBe(404);
     });
 
-    it('Checks if server returns 400 if no params provided', async () => {
-        const response = await request.get(route);
-        expect(response.statusCode).toBe(400);
+    it('returns 400 if no params provided', async () => {
+        await request.get(route).expect(400);
+    });
+
+    it('returns 404 if invalid image provided', async () => {
+        await request.get(route).query({ filename: 'missing.jpg' }).expect(404);
+    });
+
+    describe('Tests with valid image', () => {
+        it('returns 200 if valid image provided', async () => {
+            await request.get(route).query({ filename: 'fjord.jpg' }).expect(200);
+        });
+
+        it('returns jpeg content type if valid image jpeg provided', async () => {
+            await request.get(route).query({ filename: 'fjord.jpg' }).expect('Content-Type', 'image/jpeg');
+        });
+
+        it('returns png content type if valid image png provided', async () => {
+            await request.get(route).query({ filename: '685929.png' }).expect('Content-Type', 'image/png');
+        });
+
+        describe('Test with valid resize params', () => {
+            it('returns 200 if valid height and width given', async () => {
+                await request.get(route).query({ filename: 'fjord.jpg', h: '200', w: '200' }).expect(200);
+            });
+
+            it('returns png file if valid conversion params given', async () => {
+                const queryParams = { filename: 'fjord.jpg', h: '200', w: '200', ext: 'png' };
+                await request.get(route).query(queryParams).expect(200).expect('Content-Type', 'image/png');
+            });
+        });
+
+        describe('Test with invalid resize params', () => {
+            it('returns 400 if invalid height given', async () => {
+                await request.get(route).query({ filename: 'fjord.jpg', h: 'ABC', w: '200' }).expect(400);
+            });
+
+            it('returns 400 if invalid width given', async () => {
+                await request.get(route).query({ filename: 'fjord.jpg', h: '200', w: 'ABC' }).expect(400);
+            });
+
+            it('returns 400 if extension param given but resize params missing', async () => {
+                await request.get(route).query({ filename: 'fjord.jpg', ext: 'png' }).expect(400);
+            });
+        });
     });
 });
